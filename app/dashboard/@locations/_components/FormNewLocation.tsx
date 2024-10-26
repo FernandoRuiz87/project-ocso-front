6f202a4e5ce33c092b8fcb13f29d9c0472ca0760
FernandoRuiz87 <164447372+FernandoRuiz87@users.noreply.github.com>
2024-10-26 00:49:57 -0600

    feat: Delete locations and React cache

--- app/dashboard/@locations/_components/FormNewLocation.tsx
+++ app/dashboard/@locations/_components/FormNewLocation.tsx
@@ -1,11 +1,16 @@
-import { Input } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 import { createLocation } from "@/actions/locations/create";
 import axios from "axios";
 import { API_URL, TOKEN_NAME } from "@/constants";
 import { cookies } from "next/headers";
 import SelectManager from "./SelectManager";
 
-export default async function FormNewLocation() {
+export default async function FormNewLocation({
+  store,
+}: {
+  store: string | string[] | undefined;
+}) {
+  if (store) return null;
   const token = cookies().get(TOKEN_NAME)?.value;
   const responseManagers = await axios.get(`${API_URL}/managers`, {
     headers: { Authorization: `Bearer ${token}` },
@@ -14,7 +19,11 @@ export default async function FormNewLocation() {
     headers: { Authorization: `Bearer ${token}` },
   });
   return (
-    <form action={createLocation}>
+    <form
+      action={createLocation}
+      className="bg-orange-400 py-2 px-4 flex flex-col gap-6 w-full rounded-l"
+    >
+      <h1 className="text-3xl text-white">Crear tienda</h1>
       <Input
         label="Nombre"
         placeholder="Ocso Jurikiya"
@@ -31,7 +40,9 @@ export default async function FormNewLocation() {
         managers={responseManagers.data}
         locations={responseLocations.data}
       ></SelectManager>
-      <button>Subir</button>
+      <Button type="submit" color="primary">
+        Subir
+      </Button>
     </form>
   );
 }
