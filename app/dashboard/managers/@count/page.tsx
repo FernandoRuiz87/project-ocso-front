a9d6578ff9914a41000b30c405260e2b0bf80f6b
FernandoRuiz87 <164447372+FernandoRuiz87@users.noreply.github.com>
2024-10-26 19:18:35 -0600

    feat: New manager routes

---
+++ app/dashboard/managers/@count/page.tsx
@@ -0,0 +1,34 @@
+import { API_URL } from "@/constants";
+import { Manager } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
+import { Card } from "@nextui-org/react";
+
+export default async function CountManagersPage() {
+  const response = await fetch(`${API_URL}/managers`, {
+    method: "GET",
+    headers: { ...authHeaders() },
+    next: { tags: ["dashboard:managers"] },
+  });
+
+  const managers = await response.json();
+  const countNotStore = managers.filter(
+    (manager: Manager) => !manager.location
+  ).length;
+  let max: number = 0;
+  let salary = 0;
+  managers.forEach((manager: Manager) => {
+    if (manager.managerSalary > max) max = manager.managerSalary;
+    salary += manager.managerSalary;
+  });
+
+  return (
+    <Card className="w-fit px-2 py-4 text-center">
+      <h1>
+        Hay {managers.length} manager{managers.length > 1 ? "s" : ""}
+      </h1>
+      <h1>Hay {countNotStore} sin tienda</h1>
+      <h1>El salario maximo es {max}</h1>
+      <h1>El salario promedio es {salary / managers.length}</h1>
+    </Card>
+  );
+}
--- app/dashboard/managers/[id]/page.tsx
+++ app/dashboard/managers/[id]/page.tsx
@@ -2,6 +2,7 @@ import { API_URL } from "@/constants";
 import { Manager } from "@/entities";
 import { authHeaders } from "@/helpers/authHeaders";
 import { Card, CardBody, CardHeader, Divider } from "@nextui-org/react";
+import ManagerCard from "./_components/ManagerCard";
 
 export default async function ManagerPage({
   params,
@@ -14,42 +15,9 @@ export default async function ManagerPage({
     next: { tags: [`dashboard:managers:${params.id}`, "dashboard:managers"] },
   });
   const data: Manager = await response.json();
-  console.log(data);
   return (
-    <Card className="mx-20 py-2 bg-orange-50">
-      <CardHeader>
-        <p className="w-full">
-          Nombre: <b>{data.managerFullName}</b>
-        </p>
-      </CardHeader>
-      <Divider></Divider>
-      <CardBody>
-        <p className="w-full">
-          Email: <b>{data.managerEmail}</b>
-        </p>
-        <p className="w-full">
-          Teléfono: <b>{data.managerPhoneNumber}</b>
-        </p>
-        <p>
-          {data.location ? (
-            <>
-              <p>
-                Tienda: <b>{data.location.locationName}</b>
-              </p>
-              <iframe
-                className="border-2 border-orange-800 rounded-md my-2"
-                width="300"
-                height="200"
-                loading="lazy"
-                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBdX7XWCoFOxP8N1z-nTEGz4v4mvrPvvFQ
-            &q=${data.location.locationLatLng[0]},${data.location.locationLatLng[1]}`}
-              ></iframe>
-            </>
-          ) : (
-            <p>No tiene tienda</p>
-          )}
-        </p>
-      </CardBody>
-    </Card>
+    <div>
+      <ManagerCard manager={data}></ManagerCard>
+    </div>
   );
 }
