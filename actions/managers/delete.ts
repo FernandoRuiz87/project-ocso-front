06fab6af497f7311d84c24768789bc822dce7069
FernandoRuiz87 <164447372+FernandoRuiz87@users.noreply.github.com>
2024-10-29 04:37:59 -0600

    Borrar manager

--- actions/managers/delete.ts
+++ actions/managers/delete.ts
@@ -3,6 +3,7 @@
 import { API_URL } from "@/constants";
 import { authHeaders } from "@/helpers/authHeaders";
 import { revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
 
 export default async function deleteManager(
   managerId: string,
@@ -12,10 +13,13 @@ export default async function deleteManager(
   for (const key of formData.keys()) {
     manager[key] = formData.get(key);
   }
-  const response = await fetch(`${API_URL}/managers${managerId}`, {
+  const response = await fetch(`${API_URL}/managers/${managerId}`, {
     method: "DELETE",
-    headers: { ...authHeaders },
+    headers: { ...authHeaders() },
   });
 
-  if (response.status === 200) revalidateTag("dashboard:managers");
+  if (response.status === 200) {
+    revalidateTag("dashboard:managers");
+    redirect("dashboard:managers");
+  }
 }
