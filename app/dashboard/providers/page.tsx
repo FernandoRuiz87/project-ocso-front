7efc9ebeafd71f7140bb0334e7c5a402a2cebaf3
FernandoRuiz87 <164447372+FernandoRuiz87@users.noreply.github.com>
2024-10-31 17:29:21 -0600

    Pagina de proveedores

--- app/dashboard/providers/page.tsx
+++ app/dashboard/providers/page.tsx
@@ -1,5 +1,35 @@
-const ProviderPage = () => {
-  return null;
+import { API_URL } from "@/constants";
+import { Provider } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
+import ProviderCard from "./_components/ProviderCard";
+import Link from "next/link";
+import { Button } from "@nextui-org/react";
+import { LuPlus } from "react-icons/lu";
+
+const ProviderPage = async () => {
+  const response = await fetch(`${API_URL}/providers`, {
+    headers: { ...authHeaders() },
+  });
+  const providers: Provider[] = await response.json();
+
+  return (
+    <div className="flex flex-grow-0 flex-col h-[90vh] items-end w-full pt-10 px-10">
+      <Button className="w-fit" color="primary">
+        <LuPlus size={20}></LuPlus>
+      </Button>
+      <div className="flex flex-wrap w-full py-20 flex-grow-0 gap-14 ">
+        {providers.map((provider: Provider) => (
+          <Link
+            className="hover:scale-110 transition-transform"
+            href={{ pathname: `/dashboard/providers/${provider.providerId}` }}
+            key={provider.providerId}
+          >
+            <ProviderCard provider={provider} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProviderPage;
