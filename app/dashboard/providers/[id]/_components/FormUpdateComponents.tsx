d9851fde5529263959542e23b70868d76dc94ceb
FernandoRuiz87 <164447372+FernandoRuiz87@users.noreply.github.com>
2024-10-31 18:59:26 -0600

    Actualizar provider

---
+++ app/dashboard/providers/[id]/_components/FormUpdateComponents.tsx
@@ -0,0 +1,52 @@
+import updateProvider from "@/actions/providers/update";
+import { Provider } from "@/entities";
+import { Button, Input } from "@nextui-org/react";
+
+export default function FormUpdateProvider({
+  provider,
+}: {
+  provider: Provider;
+}) {
+  const { providerId } = provider;
+  const updateProviderWithId = updateProvider.bind(null, providerId);
+  return (
+    <>
+      <form
+        action={updateProviderWithId}
+        className="flex flex-wrap gap-4 flex-grow-0 bg-orange-200 rounded-md px-10 py-10 mr-10 items-center justify-center"
+      >
+        <Input
+          className="max-w-[250px]"
+          defaultValue={provider.providerName}
+          required={true}
+          isRequired
+          label="Nombre del provedor"
+          placeholder="Coca-cola"
+          name="providerName"
+        />
+        <Input
+          className="max-w-[250px]"
+          defaultValue={provider.providerEmail}
+          required={true}
+          isRequired
+          label="Correo electrónico del provedor"
+          placeholder="Coca-Cola@bussiness.com"
+          name="providerEmail"
+          type="email"
+        />
+        <Input
+          className="max-w-[250px]"
+          defaultValue={provider.providerPhoneNumber}
+          required={true}
+          isRequired
+          label="Número de teléfono del provedor"
+          placeholder="442 123 4567"
+          name="providerPhoneNumber"
+        />
+        <Button color="primary" type="submit">
+          Actualizar
+        </Button>
+      </form>
+    </>
+  );
+}
--- app/dashboard/providers/[id]/page.tsx
+++ app/dashboard/providers/[id]/page.tsx
@@ -4,6 +4,7 @@ import ProviderCard from "../_components/ProviderCard";
 import { Product, Provider } from "@/entities";
 import ProductCard from "./_components/ProductCard";
 import Link from "next/link";
+import FormUpdateProvider from "./_components/FormUpdateComponents";
 
 export default async function ProvidersPage({
   params,
@@ -14,11 +15,15 @@ export default async function ProvidersPage({
     await fetch(`${API_URL}/providers/${params.id}`, {
       method: "GET",
       headers: { ...authHeaders() },
+      next: { tags: [`dashboard:providers:${params.id}`] },
     })
   ).json();
   return (
     <div className="flex flex-grow-0 flex-col pl-10 gap-10 h-[90vh] pt-10">
-      <ProviderCard provider={provider} />
+      <div className="flex flex-row items-center gap-6">
+        <ProviderCard provider={provider} />
+        <FormUpdateProvider provider={provider}></FormUpdateProvider>
+      </div>
       <div className="h-1 bg-orange-900 w-[85vw]" />
       <div className="flex flex-wrap gap-10">
         {provider.products.map((product: Product) => (
